9f6a92ce8a8f6fabdbf27cabc7a1c917889ab23c
EzequielLassalle <ezelassalle9@gmail.com>
2024-03-13 11:59:34 -0300

    ColisionesMal

--- space.js
+++ space.js
@@ -10,7 +10,7 @@ let imagenNave;
 let enemigos = [];
 let numeroEnemigos = 0;
 let columnasEnemigos = 2;
-let filasEnemigos = 15;
+let filasEnemigos = 2;
 
 let balas1 = [];
 let balas2 = [];
@@ -47,10 +47,6 @@ window.onload = function(){
     document.addEventListener("keyup",disparar);
 
     
-
-    
-
-
 }
 
 function crearNaves(){
@@ -103,6 +99,8 @@ function actualizar(){
         canvas.fillStyle = "red"
         canvas.fillRect(balaD.posicionX,balaD.posicionY,balaD.ancho,balaD.alto);
 
+        verificarAtaque(balaD)
+
     }
 
     for(let i = 0;i<balas1.length;i++){
@@ -111,11 +109,37 @@ function actualizar(){
         canvas.fillStyle = "blue"
         canvas.fillRect(balaA.posicionX,balaA.posicionY,balaA.ancho,balaA.alto);
 
+        verificarAtaque(balaA)
+        
+
+    }
+
+    while(balas1.length > 0 && (balas1[0].usada || balas1[0].posicionY < 0)){
+        balas1.shift();
+    }
+
+    while(balas2.length > 0 && (balas2[0].usada || balas2[0].posicionY < 0)){
+        balas2.shift();
     }
 
     
     moverEnemigos();
     requestAnimationFrame(actualizar);
+
+}
+
+function verificarAtaque(bala){
+
+    for(let j = 0;j<numeroEnemigos;j++){
+        let enemigo = enemigos[j]
+        if(detectarColisiones(bala,enemigo) && enemigo.vivo && !bala.usada){
+            bala.usada = true;
+            enemigo.vivo = false;
+            numeroEnemigos--;
+            break;
+        }
+    }
+
 
 }
 
@@ -223,7 +247,7 @@ function crearEnemigos(){
         for(let j = 0;j<columnasEnemigos;j++){
             let enemigo = {
                 imagen: enemigoImagen,
-                posicionX: pantallaAncho / 2 + i*60 - 450,
+                posicionX: pantallaAncho / 2 + i*50 - 450,
                 posicionY: 50 + j * 60 ,
                 ancho: 50 ,
                 alto: 50,
@@ -241,6 +265,34 @@ function crearEnemigos(){
         }
 
     }
+
+}
+
+function detectarColisiones(bloqueA,bloqueB){
+
+    let colisiones = 0;
+
+    if(bloqueA.posicionX < bloqueB.posicionX + bloqueB.ancho){
+        colisiones++;
+    }
+
+    if(bloqueA.posicionX + bloqueA.ancho > bloqueB.posicionX){
+        colisiones++;
+    }
+
+    if(bloqueA.posicionY + bloqueA.alto > bloqueB.posicionY){
+        colisiones++;
+    }
+
+    if(bloqueA.posicionY < bloqueB.posicionY + bloqueB.alto){
+        colisiones++;
+    }
+
+    if(colisiones == 4){
+        return true;
+    }
+
+    return false;
 
 }
 
